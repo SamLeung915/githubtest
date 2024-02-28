696402912dab6e9bd20217530fa3d3246e7eb019
SamLeung915 <samleunghosum@yahoo.com.hk>
2024-02-28 20:27:11 +1100

    Updates to code

--- contour detection.py
+++ contour detection.py
@@ -1,19 +1,40 @@
 import numpy as np
 import cv2 as cv
-img = cv.imread('star.jpg', cv.IMREAD_GRAYSCALE)
-assert img is not None, "file could not be read, check with os.path.exists()"
-ret,thresh = cv.threshold(img,127,255,0)
-contours,hierarchy = cv.findContours(thresh, 1, 2)
-cnt = contours[0]
-M = cv.moments(cnt)
-print( M )
+import imutils
 
-cx = int(M['m10']/M['m00'])
-cy = int(M['m01']/M['m00'])
+image = cv.imread('frame_2.jpg', cv.IMREAD_GRAYSCALE)
+assert image is not None, "file could not be read, check with os.path.exists()"
+resized = imutils.resize(image, width = 300)
+ratio = image.shape[0] / float(resized.shape[0])
 
-area = cv.contourArea(cnt)
+#ret,thresh = cv.threshold(image,127,255,0)
+ret, thresh = cv.threshold(image,127,255,0)
 
-perimeter = cv.arcLength(cnt,True)
+#contours,hierarchy = cv.findContours(thresh, 1, 2)
+cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+#cnt = contours[0]
+cnts = imutils.grab_contours(cnts)
+c = cnts[0]
+print("c: ", c)
+#M = cv.moments(cnt)
+#print( M )
+M = cv.moments(c)
+c = c.astype("float")
+c *= ratio
+c = c.astype("int")
+cv.drawContours(image, [c], -1, (0, 255, 0), 2)
+cv.imshow("Image", image)
+cv.waitKey(0)
 
-epsilon = 0.1*cv.arcLength(cnt,True)
-approx = cv.approxPolyDP(cnt,epsilon,True)
+#cx = int(M['m10']/M['m00'])
+#cy = int(M['m01']/M['m00'])
+
+#area = cv.contourArea(cnt)
+
+#perimeter = cv.arcLength(cnt,True)
+
+#epsilon = 0.1*cv.arcLength(cnt,True)
+#approx = cv.approxPolyDP(cnt,epsilon,True)
+#print("contour: ", contours)
+#cv.imshow("Image", img)
+#cv.waitKey(0)
