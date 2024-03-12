152647ed3125cee193de23394a05ef559d7feccf
SamLeung915 <samleunghosum@yahoo.com.hk>
2024-03-13 10:06:00 +1100

    Update contour detection.py

--- contour detection.py
+++ contour detection.py
@@ -1,40 +1,35 @@
 import numpy as np
-import cv2 as cv
+import cv2
 import imutils
 
-image = cv.imread('frame_2.jpg', cv.IMREAD_GRAYSCALE)
-assert image is not None, "file could not be read, check with os.path.exists()"
-resized = imutils.resize(image, width = 300)
+# load the image and resize it to a smaller factor so that
+# the shapes can be approximated better
+image = cv2.imread("frame_2.jpg")
+resized = imutils.resize(image, width=300)
 ratio = image.shape[0] / float(resized.shape[0])
 
-#ret,thresh = cv.threshold(image,127,255,0)
-ret, thresh = cv.threshold(image,127,255,0)
-
-#contours,hierarchy = cv.findContours(thresh, 1, 2)
-cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-#cnt = contours[0]
+# convert the resized image to grayscale, blur it slightly,
+# and threshold it
+gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+# find contours in the thresholded image
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
-c = cnts[0]
-print("c: ", c)
-#M = cv.moments(cnt)
-#print( M )
-M = cv.moments(c)
-c = c.astype("float")
-c *= ratio
-c = c.astype("int")
-cv.drawContours(image, [c], -1, (0, 255, 0), 2)
-cv.imshow("Image", image)
-cv.waitKey(0)
 
-#cx = int(M['m10']/M['m00'])
-#cy = int(M['m01']/M['m00'])
+for c in cnts:
+	# compute the center of the contour, then detect the name of the
+	# shape using only the contour
+	M = cv2.moments(c)
+	cX = int((M["m10"] / M["m00"]) * ratio)
+	cY = int((M["m01"] / M["m00"]) * ratio)
+	# multiply the contour (x, y)-coordinates by the resize ratio,
+    # then draw the contours and the name of the shape on the image
+	c = c.astype("float")
+	c *= ratio
+	c = c.astype("int")
+	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
 
-#area = cv.contourArea(cnt)
-
-#perimeter = cv.arcLength(cnt,True)
-
-#epsilon = 0.1*cv.arcLength(cnt,True)
-#approx = cv.approxPolyDP(cnt,epsilon,True)
-#print("contour: ", contours)
-#cv.imshow("Image", img)
-#cv.waitKey(0)
+	# show the output image
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
