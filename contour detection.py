73ee55843a161d270bcdb1c59beae026a1199421
SamLeung915 <samleunghosum@yahoo.com.hk>
2024-03-26 20:20:56 +1100

    Update contour detection.py

--- contour detection.py
+++ contour detection.py
@@ -14,24 +14,32 @@ image = cv2.imread("test_frame_3_20.03.jpg")
 # image = cv2.imread("test_frame_13.03.jpg")
 # image = cv2.imread("test_frame_2_13.03.jpg")
 # image = cv2.imread("frame_2.jpg")
+cv2.imshow("OG", image)
+cv2.waitKey(0)
+
 resized = imutils.resize(image, width=300)
+cv2.imshow("Resized", resized)
+cv2.waitKey(0)
 ratio = image.shape[0] / float(resized.shape[0])
 
 # convert the resized image to grayscale, blur it slightly,
 # and threshold it
 gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Grayed", gray)
+cv2.waitKey(0)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 #thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
                                 #This is a threshold value i.e, any pixel that has colour value above this value will be made into the specified pixel colour (value to the right)
+cv2.imshow("Blurred", blurred)
+cv2.waitKey(0)
+
 thresh = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY)[1] #This is probably key
 cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
 
 # find contours in the thresholded image
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
-
-#print("cnts length: ", len(cnts))
-#print("cnts 7 : ", cnts[7])
 
 for c in cnts:
 	# compute the center of the contour, then detect the name of the
@@ -40,13 +48,15 @@ for c in cnts:
 	cX = int((M["m10"] / M["m00"]) * ratio)
 	cY = int((M["m01"] / M["m00"]) * ratio)
 	# multiply the contour (x, y)-coordinates by the resize ratio,
-    # then draw the contours and the name of the shape on the image
+	# then draw the contours and the name of the shape on the image
 	c = c.astype("float")
 	c *= ratio
 	c = c.astype("int")
 	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
 	perimeter = cv2.arcLength(c,True)
-	print("This is the perimeter of the print: ", perimeter)
+	area = cv2.contourArea(c)
+	print(f"This is the perimeter of the print: {perimeter}cm")
+	print(f"This is the area of the print: {area}cm")
 
 	# show the output image
 	cv2.imshow("Image", image)
