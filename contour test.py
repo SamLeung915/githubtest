5b36265e469e3fcb97df61363be08584eec0bc7d
SamLeung915 <samleunghosum@yahoo.com.hk>
2024-05-08 16:11:59 +1000

    Update contour test.py

--- contour test.py
+++ contour test.py
@@ -1,13 +1,25 @@
 import numpy as np
 import cv2
+import imutils
 
 canvas = np.zeros((500, 500), dtype=np.uint8)
 circle = cv2.circle(canvas, (250, 250) , 100, 10, -1)
+cv2.imshow("Circle", circle)
+cv2.waitKey(3000)
 
-contours, hierarchy = cv2.findContours(circle, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+blurred = cv2.GaussianBlur(circle, (5, 5), 0)
+thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY)[1] #This is probably key
+cv2.imshow("thresh", thresh)
+cv2.waitKey(0)
 
-for cnt in contours:
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+for cnt in cnts:
     circumference = cv2.arcLength(cnt, True)
     area = cv2.contourArea(cnt)
     print("c: %s" % circumference)
     print("area: %s" % area)
+    final_image = cv2.drawContours(circle, [cnt], -1, (0, 255, 0), 2)
+    cv2.imshow("Final image", final_image)
+    cv2.waitKey(0)
