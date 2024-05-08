018f2e001b1502f26434b3fcd13cd6d73166328b
Zaroof2003 <szarif395@gmail.com>
2024-05-08 16:10:55 +1000

    Added Cropping for whole folder

--- crop.py
+++ crop.py
@@ -1,22 +1,44 @@
 from PIL import Image 
+import os
 
-im = Image.open('frame_2.jpg')
-width, height = im.size
+current_directory = os.getcwd()
 
-print('Width', width)
-print('Height', height)
+output_folder = "out"
+out_path = os.path.join(current_directory, output_folder)
 
-new_width = width * 0.5
-new_height = height * 0.5
+folder_name = "cube"
+folder_path = os.path.join(current_directory, folder_name)
+os.chdir(folder_path)
 
-# Calculate the coordinates for cropping
-left = (width - new_width) / 2
-top = (height - new_height) / 2
-right = (width + new_width) / 2
-bottom = (height + new_height) / 2
 
-# Crop the image
-im_cropped = im.crop((left, top, right, bottom))
 
-# Save the cropped image
-im_cropped.save('cropped_Chicken.png')
+i = 1
+# Loop through each file in the directory
+for filename in os.listdir(folder_path):
+    # Check if the file is an image (you may need more robust checks depending on your use case)
+    if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg'):
+        # Process your image here, for example, print its filename
+        print("Processing image:", filename)
+
+        im = Image.open(filename)
+        width, height = im.size
+
+        print('Width', width)
+        print('Height', height)
+
+        new_width = width * 0.5
+        new_height = height * 0.5
+
+        # Calculate the coordinates for cropping
+        left = (width - new_width) / 2
+        top = (height - new_height) / 2
+        right = (width + new_width) / 2
+        bottom = (height + new_height) / 2
+
+        # Crop the image
+        im_cropped = im.crop((left, top, right, bottom))
+
+        output = f"out_{i}.png"
+        i += 1
+        # Save the cropped image
+        im_cropped.save(os.path.join(out_path, output))
